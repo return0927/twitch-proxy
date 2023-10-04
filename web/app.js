c02a3bbf5ee4a42f6c0a44402459475a2faca531
Eunhak Lee <lee@enak.kr>
2023-10-04 23:32:24 +0900

    use insecure http connection
    This can be used when behind a proxy

--- web/app.js
+++ web/app.js
@@ -1,13 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
-import fs from 'fs';
-import https from 'https';
-
-const TLS_OPTIONS = {
-  cert: fs.readFileSync('./tls/cert.pem'),
-  key: fs.readFileSync('./tls/key.pem')
-}
 
 const app = express();
 
@@ -33,4 +26,4 @@ app.get('/video-edge-*', async (req, res) => {
   res.redirect(proxyURL);
 });
 
-https.createServer(TLS_OPTIONS, app).listen(3000);
+app.listen(3000);
